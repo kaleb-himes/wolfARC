09bbf3a3eae82e542e0a5bb212bb626bb8bb8456
kaleb-himes <kaleb@wolfssl.com>
2016-01-25 17:06:12 -0700

    Step 3 nearly complete

--- step_3_check_example_cert_and_crl_dates.sh
+++ step_3_check_example_cert_and_crl_dates.sh
@@ -39,32 +39,58 @@ month_by_num() {
     fi
 }
 
+process_date() {
+    # Extract just the date from the line containing the "Not After" date
+    E_DATE=`echo "$1" | rev | cut -f1-3 -d: | rev`
+
+    # Extract just the month from the extracted date
+    CHECK_MONTH=`echo "$E_DATE" | cut -f1-2 -d ' ' | rev | cut -f1 -d ' ' | rev`
+    month_by_num $CHECK_MONTH
+    CHECK_MONTH_NUM=$MONTH
+
+    # Extract just the year from the extracted date
+    CHECK_YEAR=`echo "$E_DATE" | cut -f1-5 -d ' ' | rev | cut -f1 -d ' ' | rev`
+
+    # Process the date
+    DIFF_YEAR=$(( CHECK_YEAR - NOW_YEAR ))
+    DIFF_MONTH=$(( CHECK_MONTH_NUM - NOW_MONTH_NUM ))
+    if [ $DIFF_YEAR -eq 0 ]; then
+        if [ $DIFF_MONTH -le 4 ]; then
+            echo "Certificates need to be updated"
+            set_failure 5
+        else
+            echo "Certificate will expire in $DIFF_MONTH months"
+        fi
+    else
+        if [ $DIFF_YEAR -lt 0 ]; then
+            echo "This must be one of the expired certificates used for testing."
+        else
+            echo "Certificate will expire in $DIFF_YEAR year(s) and $DIFF_MONTH month(s)"
+        fi
+    fi
+}
+
+
 CURR_DIR=`pwd`
-NOW="$(date +'%b %d %H:%M:%S %Y')"
+echo "#-------------------------------------------------------------------#"
+echo " Checking the dates on the certificates and crl's"
+echo "#-------------------------------------------------------------------#"
 NOW_YEAR="$(date +'%Y')"
+echo "The current year is: $NOW_YEAR"
 NOW_MONTH="$(date +'%b')"
-echo "Todays date is:     $NOW GMT"
-echo "NOW_YEAR:  $NOW_YEAR"
-echo "NOW_MONTH: $NOW_MONTH"
+echo "The current month is: $NOW_MONTH"
 month_by_num $NOW_MONTH
-NOW_MONTH=$MONTH
-echo "NOW_MONTH: $NOW_MONTH"
+NOW_MONTH_NUM=$MONTH
+echo "Current month in decimal format is: $NOW_MONTH_NUM"
 
 # Extract the expiration dates for certificates
-grep -r "$CURR_DIR/wolfssl/certs/" -e "Not After.*" | while read -r line; do
-    E_DATE=`echo "$line" | rev | cut -f1-3 -d: | rev`
-    echo "EXTRACTED DATE IS: $E_DATE"
-    MONTH=`echo "$E_DATE" | cut -f1-2 -d ' ' | rev | cut -f1 -d ' ' | rev`
-    echo "MONTH = $MONTH"
-    month_by_num $MONTH
-    YEAR=`echo "$E_DATE" | cut -f1-5 -d ' ' | rev | cut -f1 -d ' ' | rev`
-    echo "YEAR  = $YEAR"
+grep -r "$CURR_DIR/wolfssl/certs/" -e "Not After" | while read -r line; do
+    process_date "$line"
 done
 
 # Extract the expiration dates for crl's
 grep -r "$CURR_DIR/wolfssl/certs/" -e "Next Update" | while read -r line; do
-    E_DATE=`echo "$line" | rev | cut -f1-3 -d: | rev`
-    echo "EXTRACTED DATE IS: $E_DATE"
+    process_date "$line"
 done
 
 
